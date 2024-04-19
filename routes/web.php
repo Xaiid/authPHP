3a03e176c76713a6eb81bd05a5a4c87435b9c11b
Jeaneth Farmer <jeaneth.farmer@gmail.com>
2024-04-19 14:33:49 +0200

    Added login with beeze pkg

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,25 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Models\Member;
 
-Route::get('/dashboard', function () {
+Route::get('/', function () {
     return view('welcome');
+})->middleware(['auth', 'verified'])->name('welcome');
+
+Route::get('/members', function () {
+    return Member::all();
+})->middleware(['auth', 'verified'])->name('members');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/members', function() { 
-    return Member::all();
-});
+require __DIR__.'/auth.php';
